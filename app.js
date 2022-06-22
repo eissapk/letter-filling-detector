9be935664480dbc254eab9769348b38faadbc084
Eissa Saber <eissapk44@gmail.com>
2022-06-22 22:35:50 +0200

    automated letters drawing

--- app.js
+++ app.js
@@ -1,125 +1,8 @@
 let painting = false;
-let eraserToggle = true;
 let penWidth = 4;
-let penColor = "blue";
-let lastWidth;
-let lastColor;
-let timeout = [];
-// select canvas
-const canvas1 = document.querySelector("#one");
-const ctx1 = canvas1.getContext("2d");
-const w = 1000;
-const h = 500;
-canvas1.width = w;
-canvas1.height = h;
-
-//! try with rectangle
-// draw white firstly
-// ctx1.rect(0, 0, 200, 200);
-// ctx1.fillStyle = "white";
-// ctx1.fill();
-// ctx1.beginPath();
-// draw desired color secondly
-// ctx1.rect(0, 0, 100, 100);
-// ctx1.fillStyle = "#FF0000";
-// ctx1.fill();
-
-//! try with text
-// draw white background
-ctx1.rect(0, 0, 1000, 500);
-ctx1.fillStyle = "white";
-ctx1.fill();
-ctx1.beginPath();
-
-const font = "150px serif";
-const lineWidth = 5;
-const textAlign = "left";
-const letter = ["ضـ ـضـ ـض", 0, 100];
-
-// draw text as fallback to fill the space between dashes
-ctx1.font = font;
-ctx1.lineWidth = lineWidth;
-ctx1.textAlign = textAlign;
-ctx1.fillStyle = "#c7c7c7";
-ctx1.fillText(...letter);
-ctx1.beginPath();
-
-// // draw desired text
-// ctx1.font = font;
-// ctx1.lineWidth = 2;
-// ctx1.textAlign = textAlign;
-// ctx1.setLineDash([5, 7]);
-// ctx1.strokeStyle = "#000";
-// ctx1.strokeText(...letter);
-
-const data = getNonWhitePos(ctx1, w, h);
-console.log("getNonWhitePos", data);
-
-// ------------
-
-const canvas2 = document.querySelector("#two");
-canvas2.width = window.innerWidth - 20;
-canvas2.height = window.innerHeight - 20;
-const ctx2 = canvas2.getContext("2d");
-
-// mouse position
-const mousePos = e => {
-  let status = e.type == "mousedown" ? true : false;
-  if (status) {
-    // mouse down
-    painting = true;
-    draw_via_mouse(e);
-  } else {
-    // mouse up
-    painting = false;
-    ctx2.beginPath();
-  }
-};
-
-// draw via mouse
-const draw_via_mouse = e => {
-  // prevent default behavior
-  e.preventDefault();
-
-  // !start getting cursor pos
-  e = e || window.event;
-  let a = e.target.getBoundingClientRect();
-  let x = e.pageX - a.left;
-  let y = e.pageY - a.top;
-  // !end getting cursor pos
-  if (!painting) return;
-
-  ctx2.lineWidth = penWidth;
-  ctx2.lineCap = "round";
-  ctx2.strokeStyle = penColor;
-  ctx2.lineTo(x, y);
-  ctx2.stroke();
-  ctx2.beginPath();
-  ctx2.moveTo(x, y);
-  // console.log(ctx1.isPointInPath(x, y)); // this is used with rect methods only not with text
-  console.log({ x, y });
-
-  const obj = data.find(item => item.x === x && item.y === y);
-  console.log(obj);
-
-  if (obj) {
-    console.warn("inside");
-  } else {
-    console.warn("outside");
-    const time = setTimeout(() => {
-      alert("you are ouside the letter, start over");
-      location.reload();
-    }, 200);
-    timeout.push(time);
-    timeout.forEach((item, index, array) => {
-      if (index !== array.length - 1) clearTimeout(item);
-    });
-  }
-};
-
-canvas2.addEventListener("mousedown", mousePos);
-canvas2.addEventListener("mouseup", mousePos);
-canvas2.addEventListener("mousemove", draw_via_mouse);
+let penColor = "black";
+const originsDataArr = [];
+const origins = document.querySelectorAll(".origin");
 
 function getNonWhitePos(ctx, w, h) {
   // start taken from stackoverflow
@@ -145,3 +28,114 @@ function getNonWhitePos(ctx, w, h) {
   console.log({ fullData: data });
   return data.filter(item => item.color !== "#ffffff"); // return NON-white pxs positions
 }
+
+function handelOrigin(ctx, letter, w, h, y) {
+  ctx.rect(0, 0, w, h);
+  ctx.fillStyle = "white";
+  ctx.fill();
+  ctx.beginPath();
+
+  const font = "150px serif";
+  const lineWidth = 5;
+  const textAlign = "left";
+
+  ctx.font = font;
+  ctx.lineWidth = lineWidth;
+  ctx.textAlign = textAlign;
+  ctx.fillStyle = "#c7c7c7";
+  ctx.fillText(letter, -10, y);
+  ctx.beginPath();
+
+  return getNonWhitePos(ctx, w, h);
+}
+
+origins.forEach((origin, index) => {
+  const parent = origin.parentElement;
+  const originCTX = origin.getContext("2d");
+  const letter = origin.getAttribute("data-letter");
+  const [W, H, Y] = origin.getAttribute("data-dim").split("x");
+
+  const layer = document.createElement("canvas");
+  const layerCTX = origin.getContext("2d");
+  layer.className = "layer";
+  origin.width = Number(W);
+  origin.height = Number(H);
+  layer.width = Number(W) - 4;
+  layer.height = Number(H) - 4;
+  parent.style.width = W + "px";
+  parent.style.height = H + "px";
+  origin.parentElement.append(layer);
+
+  let data = handelOrigin(originCTX, letter, Number(W), Number(H), Number(Y));
+  originsDataArr.push(data);
+  console.log("originsDataArr: getNonWhitePos", originsDataArr);
+
+  // -------------------
+
+  function reset(ctx) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  }
+
+  const mousePos = e => {
+    let status = e.type == "mousedown" ? true : false;
+    if (status) {
+      // mouse down
+      painting = true;
+      draw_via_mouse(e);
+    } else {
+      // mouse up
+      painting = false;
+      layerCTX.beginPath();
+    }
+  };
+
+  const draw_via_mouse = e => {
+    e.preventDefault();
+    const parent = e.target.parentElement;
+    e = e || window.event;
+    let x = e.clientX - parent.offsetLeft + window.scrollX;
+    let y = e.clientY - parent.offsetTop + window.scrollY;
+    if (!painting) return;
+
+    layerCTX.lineWidth = penWidth;
+    layerCTX.lineCap = "round";
+    layerCTX.strokeStyle = penColor;
+    layerCTX.lineTo(x, y);
+    layerCTX.stroke();
+    layerCTX.beginPath();
+    layerCTX.moveTo(x, y);
+
+    console.log({ x, y });
+
+    // check coords
+    const obj = originsDataArr[index].find(item => item.x === x && item.y === y);
+    console.log(obj);
+    if (obj) {
+      console.warn("inside");
+    } else {
+      console.warn("outside");
+      oConfirm({
+        title: "Hint",
+        desc: `You drew outside the letter ${letter}. Do you want to start over?`,
+        btns: { cancel: { exists: true, text: "Cancel" }, okay: { text: "Okay" } },
+      }).then(res => {
+        if (res) {
+          reset(layerCTX);
+          handelOrigin(originCTX, letter, Number(W), Number(H), Number(Y));
+        } else {
+          console.log("cancel");
+        }
+      });
+    }
+  };
+
+  // events
+  layer.onmousedown = mousePos;
+  layer.onmouseup = mousePos;
+  layer.onmousemove = draw_via_mouse;
+  layer.onmouseleave = () => {
+    painting = false;
+    layerCTX.beginPath();
+    console.log("Out Board: " + index);
+  };
+});
