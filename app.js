e961e89156eaca32d4379e27003397f71d29fda7
Eissa Saber <eissapk44@gmail.com>
2022-06-26 19:52:31 +0200

    added custom font and centered text in canvas

--- app.js
+++ app.js
@@ -29,24 +29,45 @@ function getNonWhitePos(ctx, w, h) {
   return data.filter(item => item.color !== "#ffffff"); // return NON-white pxs positions
 }
 
-function handelOrigin(ctx, letter, w, h, y) {
+function handelOrigin(ctx, letter, w, h, y, cb) {
+  console.log(ctx);
   ctx.rect(0, 0, w, h);
   ctx.fillStyle = "white";
   ctx.fill();
   ctx.beginPath();
 
-  const font = "150px Aerial";
-  const lineWidth = 5;
-  const textAlign = "left";
+  // draw text
+  var myFont = new FontFace("Cairo", "url(/font.woff2)");
+  myFont.load().then(function (font) {
+    document.fonts.add(font);
+    const textAlign = "center";
+    ctx.font = "bold 65px Cairo";
+    ctx.textAlign = textAlign;
+    ctx.fillStyle = "#c7c7c7";
+    ctx.fillText(letter, ctx.canvas.width / 2, ctx.canvas.height / 2);
+    ctx.beginPath();
+    // get text metrics
+    var metrics = ctx.measureText(letter);
+    let fontHeight = metrics.fontBoundingBoxAscent + metrics.fontBoundingBoxDescent;
+    let actualHeight = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+    var width = metrics.width;
 
-  ctx.font = font;
-  ctx.lineWidth = lineWidth;
-  ctx.textAlign = textAlign;
-  ctx.fillStyle = "#c7c7c7";
-  ctx.fillText(letter, -10, y);
-  ctx.beginPath();
+    // re-draw based on known metrics
+    // white background
+    ctx.rect(0, 0, w, h);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.beginPath();
+    // text
+    ctx.font = "bold 65px Cairo";
+    ctx.textAlign = textAlign;
+    ctx.fillStyle = "#c7c7c7";
+    ctx.fillText(letter, ctx.canvas.width / 2, ctx.canvas.height / 2 + actualHeight / 2);
+    ctx.beginPath();
 
-  return getNonWhitePos(ctx, w, h);
+    console.log({ width, actualHeight, fontHeight });
+    if (cb) cb(getNonWhitePos(ctx, w, h));
+  });
 }
 
 origins.forEach((origin, index) => {
@@ -66,133 +87,136 @@ origins.forEach((origin, index) => {
   parent.style.height = H + "px";
   origin.parentElement.append(layer);
 
-  let data = handelOrigin(originCTX, letter, Number(W), Number(H), Number(Y));
-  originsDataArr.push(data);
-  console.log("originsDataArr: getNonWhitePos", originsDataArr);
+  handelOrigin(originCTX, letter, Number(W), Number(H), Number(Y), function (data) {
+    originsDataArr.push(data);
+    console.log("originsDataArr: getNonWhitePos", originsDataArr);
 
-  // -------------------
+    // -------------------
 
-  function reset(ctx) {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  }
+    function reset(ctx) {
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    }
 
-  const mousePos = e => {
-    let status = e.type == "mousedown" ? true : false;
-    if (status) {
-      // mouse down
-      painting = true;
-      draw_via_mouse(e);
-    } else {
-      // mouse up
+    const mousePos = e => {
+      let status = e.type == "mousedown" ? true : false;
+      if (status) {
+        // mouse down
+        painting = true;
+        draw_via_mouse(e);
+      } else {
+        // mouse up
+        painting = false;
+        layerCTX.beginPath();
+      }
+    };
+
+    const touchPos = e => {
+      let status = e.type == "touchstart" ? true : false;
+      if (status) {
+        // touch start
+        painting = true;
+        draw_via_touch(e);
+      } else {
+        // touch end
+        painting = false;
+        layerCTX.beginPath();
+      }
+    };
+
+    const draw_via_mouse = e => {
+      const parent = e.target.parentElement;
+      e = e || window.event;
+      let x = e.clientX - parent.offsetLeft + window.scrollX;
+      let y = e.clientY - parent.offsetTop + window.scrollY;
+
+      console.log({ x, y });
+      if (!painting || x <= 0 || x >= e.target.offsetWidth || y <= 0 || y >= e.target.offsetHeight) return (painting = false);
+
+      layerCTX.lineWidth = penWidth;
+      layerCTX.lineCap = "round";
+      layerCTX.strokeStyle = penColor;
+      layerCTX.lineTo(x, y);
+      layerCTX.stroke();
+      layerCTX.beginPath();
+      layerCTX.moveTo(x, y);
+
+      // check coords
+      const obj = originsDataArr[index].find(item => item.x === x && item.y === y);
+      console.log(obj);
+      if (obj) {
+        console.warn("inside");
+      } else {
+        console.warn("outside");
+        oConfirm({
+          title: "Hint",
+          desc: `You drew outside the letter ${letter}. Do you want to start over?`,
+          btns: { cancel: { exists: true, text: "Cancel" }, okay: { text: "Okay" } },
+        }).then(res => {
+          if (res) {
+            reset(layerCTX);
+            handelOrigin(originCTX, letter, Number(W), Number(H), Number(Y));
+          } else {
+            console.log("cancel");
+          }
+        });
+      }
+    };
+
+    const draw_via_touch = e => {
+      const parent = e.target.parentElement;
+      e = e || window.event;
+      let x = e.touches[0].pageX - parent.offsetLeft + window.scrollX;
+      let y = e.touches[0].pageY - parent.offsetTop + window.scrollY;
+      x = Math.round(x);
+      y = Math.round(y);
+
+      console.log({ x, y });
+      if (!painting || x <= 0 || x >= e.target.offsetWidth || y <= 0 || y >= e.target.offsetHeight) return (painting = false);
+
+      layerCTX.lineWidth = penWidth;
+      layerCTX.lineCap = "round";
+      layerCTX.strokeStyle = penColor;
+      layerCTX.lineTo(x, y);
+      layerCTX.stroke();
+      layerCTX.beginPath();
+      layerCTX.moveTo(x, y);
+
+      // check coords
+      const obj = originsDataArr[index].find(item => item.x === x && item.y === y);
+      console.log(obj);
+      if (obj) {
+        console.warn("inside");
+      } else {
+        console.warn("outside");
+        painting = false;
+        oConfirm({
+          title: "Hint",
+          desc: `You drew outside the letter ${letter}. Do you want to start over?`,
+          btns: { cancel: { exists: true, text: "Cancel" }, okay: { text: "Okay" } },
+        }).then(res => {
+          if (res) {
+            reset(layerCTX);
+            handelOrigin(originCTX, letter, Number(W), Number(H), Number(Y));
+          } else {
+            console.log("cancel");
+          }
+        });
+      }
+    };
+
+    // events
+    // desktop
+    layer.onmousedown = mousePos;
+    layer.onmouseup = mousePos;
+    layer.onmousemove = draw_via_mouse;
+    layer.onmouseleave = () => {
       painting = false;
       layerCTX.beginPath();
-    }
-  };
-
-  const touchPos = e => {
-    let status = e.type == "touchstart" ? true : false;
-    if (status) {
-      // touch start
-      painting = true;
-      draw_via_touch(e);
-    } else {
-      // touch end
-      painting = false;
-      layerCTX.beginPath();
-    }
-  };
-
-  const draw_via_mouse = e => {
-    const parent = e.target.parentElement;
-    e = e || window.event;
-    let x = e.clientX - parent.offsetLeft + window.scrollX;
-    let y = e.clientY - parent.offsetTop + window.scrollY;
-
-    console.log({ x, y });
-    if (!painting || x <= 0 || x >= e.target.offsetWidth || y <= 0 || y >= e.target.offsetHeight) return (painting = false);
-
-    layerCTX.lineWidth = penWidth;
-    layerCTX.lineCap = "round";
-    layerCTX.strokeStyle = penColor;
-    layerCTX.lineTo(x, y);
-    layerCTX.stroke();
-    layerCTX.beginPath();
-    layerCTX.moveTo(x, y);
-
-    // check coords
-    const obj = originsDataArr[index].find(item => item.x === x && item.y === y);
-    console.log(obj);
-    if (obj) {
-      console.warn("inside");
-    } else {
-      console.warn("outside");
-      oConfirm({
-        title: "Hint",
-        desc: `You drew outside the letter ${letter}. Do you want to start over?`,
-        btns: { cancel: { exists: true, text: "Cancel" }, okay: { text: "Okay" } },
-      }).then(res => {
-        if (res) {
-          reset(layerCTX);
-          handelOrigin(originCTX, letter, Number(W), Number(H), Number(Y));
-        } else {
-          console.log("cancel");
-        }
-      });
-    }
-  };
-
-  const draw_via_touch = e => {
-    const parent = e.target.parentElement;
-    e = e || window.event;
-    let x = e.touches[0].pageX - parent.offsetLeft + window.scrollX;
-    let y = e.touches[0].pageY - parent.offsetTop + window.scrollY;
-
-    console.log({ x, y });
-    if (!painting || x <= 0 || x >= e.target.offsetWidth || y <= 0 || y >= e.target.offsetHeight) return (painting = false);
-
-    layerCTX.lineWidth = penWidth;
-    layerCTX.lineCap = "round";
-    layerCTX.strokeStyle = penColor;
-    layerCTX.lineTo(x, y);
-    layerCTX.stroke();
-    layerCTX.beginPath();
-    layerCTX.moveTo(x, y);
-
-    // check coords
-    const obj = originsDataArr[index].find(item => item.x === x && item.y === y);
-    console.log(obj);
-    if (obj) {
-      console.warn("inside");
-    } else {
-      console.warn("outside");
-      painting = false;
-      oConfirm({
-        title: "Hint",
-        desc: `You drew outside the letter ${letter}. Do you want to start over?`,
-        btns: { cancel: { exists: true, text: "Cancel" }, okay: { text: "Okay" } },
-      }).then(res => {
-        if (res) {
-          reset(layerCTX);
-          handelOrigin(originCTX, letter, Number(W), Number(H), Number(Y));
-        } else {
-          console.log("cancel");
-        }
-      });
-    }
-  };
-
-  // events
-  // desktop
-  layer.onmousedown = mousePos;
-  layer.onmouseup = mousePos;
-  layer.onmousemove = draw_via_mouse;
-  layer.onmouseleave = () => {
-    painting = false;
-    layerCTX.beginPath();
-    console.log("Out Board: " + index);
-  };
-  // mobile
-  layer.ontouchstart = touchPos;
-  layer.ontouchend = touchPos;
-  layer.ontouchmove = draw_via_touch;
+      console.log("Out Board: " + index);
+    };
+    // mobile
+    layer.ontouchstart = touchPos;
+    layer.ontouchend = touchPos;
+    layer.ontouchmove = draw_via_touch;
+  });
 });
